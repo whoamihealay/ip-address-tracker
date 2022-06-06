328c7ce16030f24284b5e399d30bafed4cc0bd7c
Andrew Southern <whoamihealay@gmail.com>
2022-06-06 16:18:40 -0400

    updated to react 18

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
 import { GlobalStyles } from "./Global";
 import { theme } from "./theme";
 
-const container = document.getElementById("root");
+const container = document.getElementById("root") as Element;
 
-ReactDOM.render(
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <App />
     </ThemeProvider>
-  </React.StrictMode>,
-  container
+  </React.StrictMode>
 );
